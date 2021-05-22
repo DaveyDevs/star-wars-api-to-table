277d3249b7e38ce3534d3db1a61a4e518cba47de
Davey Devs <45680621+DaveyDevs@users.noreply.github.com>
2021-05-22 16:24:13 -0500

    Water surface area logic

--- swapi-app/src/components/PlanetList.js
+++ swapi-app/src/components/PlanetList.js
@@ -20,6 +20,34 @@ export function PlanetList() {
           console.error(e);
         }
       };
+
+      const getSurfaceArea = (diameter) => {
+        let radius = diameter / 2;
+        let surfaceArea = 4 * Math.PI * Math.pow(radius, 2);
+        return surfaceArea;
+      }
+
+      
+      const getWaterSurfaceArea = (diameter, surfaceWater) => {
+        console.log(diameter);
+        console.log(surfaceWater);
+
+        let surfaceArea = getSurfaceArea(diameter);
+        let surfaceWaterPercent = surfaceWater / 100;
+
+        console.log(surfaceArea);
+        console.log(surfaceWaterPercent);
+
+        let waterSurfaceArea = Math.round(surfaceArea * surfaceWaterPercent);
+
+        console.log(waterSurfaceArea);
+
+        if (isNaN(waterSurfaceArea)) {
+            return "?";
+        } else {
+            return waterSurfaceArea;
+        }
+      }
     
       useEffect(() => {
         getPlanets();
@@ -33,28 +61,37 @@ export function PlanetList() {
                     <p>Loading...</p>
                 </div>
                 :
-                <p>Planets!</p>
-            }
-            {planets && 
-                <table>
-                    <caption>Star Wars Planets List</caption>
-                    <thead>
-                        <tr>
-                        <th scope="col">Planet</th>
-                        <th scope="col">next</th>
-                        <th scope="col">next</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {planets.results.map((planet) => 
-                        <tr>
-                            <th scope="row">{planet.name}</th>
-                            <td>{planet.name}</td>
-                            <td>{planet.climate}</td>
-                        </tr>
-                        )}
-                    </tbody>
-                </table>
+                <div>
+                {planets && 
+                    <table>
+                        <caption>Star Wars Planets List</caption>
+                        <thead>
+                            <tr>
+                            <th scope="col">Planet</th>
+                            <th scope="col">Climate</th>
+                            <th scope="col">Residents</th>
+                            <th scope="col">Terrains</th>
+                            <th scope="col">Population</th>
+                            <th scope="col">Surface area covered by water (km<sup>2</sup>)</th>
+    
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {planets.results.map((planet) => 
+                            <tr key={planet.name}>
+                                <th scope="row">{planet.name}</th>
+                                <td>{planet.climate}</td>
+                                <td>{planet.residents.length}</td>
+                                <td>{planet.terrain}</td>
+                                <td>{planet.population}</td>
+                                <td>{getWaterSurfaceArea(planet.diameter, planet.surface_water)}</td>
+                                {/* <td>{Math.round(getSurfaceArea(planet.diameter) * parseFloat(planet.surface_water) / 100.0)}</td> */}
+                            </tr>
+                            )}
+                        </tbody>
+                    </table>
+                }
+                </div>
             }
         </div>
     )
